dba3212fbd5dd7c9279cf58bf174a30a3beb6ee9
venustuspl <venustus.pl@gmail.com>
2019-11-03 21:42:54 +0100

    2142

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   var apiRoot = 'https://damp-hollows-88775.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://api.trello.com/1/v1/trello/';
+  const trelloApiRoot = 'https://damp-hollows-88775.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 

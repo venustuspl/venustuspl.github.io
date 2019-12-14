023aac3b4e5b6148f381b096d8a578d01a79c714
venustuspl <47386588+venustuspl@users.noreply.github.com>
2019-12-14 21:24:31 +0100

    Add files via upload

--- localscript.js
+++ localscript.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://damp-hollows-88775.herokuapp.com/v1/';
-  const trelloApiRoot = 'https://damp-hollows-88775.herokuapp.com/v1/trello/';
+  const apiRoot = 'http://localhost:8080/v1/';
+  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
